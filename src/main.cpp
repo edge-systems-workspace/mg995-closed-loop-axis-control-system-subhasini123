252fc30197b6294e33d818a1b05e0c95f8b98a68
subhasini123 <subhasini.kumari_cs.aiml24@gla.ac.in>
2026-02-20 08:54:24 +0530

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ void setup() {
 }
 
 void loop() {
-    // Sweep from 0 to 180 degrees
+
     for (int pos = 0; pos <= 180; pos++) {
         myServo.write(pos);   // Move to position
         delay(15);            // Wait for servo to reach position
